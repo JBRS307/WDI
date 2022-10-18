bbc6563ca5176ec08b3dbb1f3314c1f119d51230
JBRS307 <kuba.rekas.307@gmail.com>
2022-10-18 23:33:38 +0200

    isPrime correction 18.10.2022

--- list1/zad8.py
+++ list1/zad8.py
@@ -2,10 +2,11 @@
 from math import isqrt
 
 def isPrime(n):
-    if n <= 1:
-        return False
     if n == 2 or n == 3:
         return True
+    
+    if n%2 == 0 or n%3 == 0 or n <= 1:
+        return False
 
     i = 5
     while i <= isqrt(n):
@@ -19,5 +20,5 @@ def isPrime(n):
     return True
 #===========================================================
 if __name__ == "__main__":
-    n = int(input().strip())
+    n = int(input())
     print(isPrime(n))
