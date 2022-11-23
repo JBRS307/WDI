ca35fcce8b8d82fe6343ffc04b6ba1ca49aad0d4
JBRS307 <kuba.rekas.307@gmail.com>
2022-11-23 21:19:44 +0100

    22.11.2022 (late)

---
+++ list6/zad4.py
@@ -0,0 +1,48 @@
+from os import system
+
+
+def print_board(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            print(board[i][j], end='\t')
+        print()
+#-----------------------------------------------------
+
+def fill_knight(board, leng, x, y, n = 1):
+    moves = ((+2, +1), (+2, -1), (-2, +1), (-2, -1),
+             (+1, +2), (+1, -2), (-1, +2), (-1, -2))
+    
+    if board[x][y] == 0:
+        board[x][y] = n
+    else:
+        return False
+
+    if n == leng*leng:
+        return True
+            
+    i = 0
+    for move in moves:
+        if x+move[0] >= 0 and x+move[0] < leng and y+move[1] >= 0 and y+move[1] < leng:
+                stop = fill_knight(board, leng, x+move[0], y+move[1], n+1)
+                if stop:
+                    return True
+    
+    board[x][y] = 0
+
+    return False
+#-----------------------------------------------------
+
+def start(leng):
+    board = [[0]*leng for _ in range(leng)]
+    res = fill_knight(board, leng,  0, 0)
+    return res, board
+#=====================================================
+
+if __name__ == "__main__":
+    system("clear")
+
+    leng = int(input())
+    possible, board = start(leng)
+
+    print_board(board)
+    print(possible)
